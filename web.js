74f73cc316ee581e2202d30fa0def5ce2497f302
Paras Jain <parasjain@parasjain.com>
2013-03-24 18:35:13 -0700

    Update web.js

--- web.js
+++ web.js
@@ -2,5 +2,5 @@ var gzippo = require('gzippo');
 var express = require('express');
 
 var app = express.createServer(express.logger());
-app.use(gzippo.staticGzip(__dirname + '/app/dist'));
+app.use(gzippo.staticGzip(__dirname + '/app'));
 app.listen(process.env.PORT || 5000);
